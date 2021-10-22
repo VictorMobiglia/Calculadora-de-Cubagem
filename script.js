d9b029b156c7ccae9b9ce82d8c381e8ae32f728d
VictorMobiglia <92962784+VictorMobiglia@users.noreply.github.com>
2021-10-21 23:23:25 -0300

    Versão 2.0
    Co-Authored-By: Gabriel Butarelo <gabrielbutarelo16@gmail.com>

--- script.js
+++ script.js
@@ -2,12 +2,19 @@ function calcular(){
 
     const elementRetorno = document.getElementById('cubagem')
 
-    const altura = document.getElementById("altura").value;
+    const altura = document.getElementById("altura").value
     const largura = document.getElementById("largura").value
     const comprimento = document.getElementById("comprimento").value
 
-    const cubagem = ((altura / 100) * (largura / 100) * (comprimento / 100)) * 300;
     
-    elementRetorno.innerText = parseFloat(cubagem).toFixed(2);
+    if(altura.length == 0 || largura.length == 0 || comprimento.length == 0){
+        document.getElementById('erro').style.display = 'block'
+        return;
+    }else{
+        const cubagem = ((altura / 100) * (largura / 100) * (comprimento / 100)) * 300;
+        elementRetorno.innerText = parseFloat(cubagem).toFixed(2)
+        document.getElementById('erro').style.display = 'none'
+        return;
+    }
 
 }
